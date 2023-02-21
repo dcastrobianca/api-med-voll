f766545e7d1d5138a7624f938a93c36320e30e75
Bianca Rodrigues de Castro <biancardc24@gmail.com>
2023-02-21 22:52:53 +0100

    GET retrive partial information with DTO and Page

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -1,7 +1,13 @@
 package med.voll.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import med.voll.api.doctor.Doctor;
+import med.voll.api.doctor.DoctorListData;
 import med.voll.api.doctor.DoctorRegistrationData;
 import med.voll.api.doctor.DoctorRepository;
 
@@ -23,6 +30,11 @@ public class DoctorController {
     @Transactional
     public void create(@RequestBody @Valid DoctorRegistrationData data){
         doctorRepository.save(new Doctor(data));
+    }
+
+    @GetMapping
+    public Page<DoctorListData> search(@PageableDefault(size = 10, sort = {"name"}) Pageable page){
+        return doctorRepository.findAll(page).map(DoctorListData::new);
     }
     
 }
